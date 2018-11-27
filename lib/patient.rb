62ce4a12ef595b896864b205722d1a86279ac25e
Robin Kim <rkim1214@gmail.com>
2018-11-27 02:32:20 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,15 @@
 class Patient 
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
+    @@all << self
   end 
   
   def self.all 
-    
+    @@all 
+  end 
   
 end 
